83f78af1ef8dd55a6bc76293fb0412301a921dc8
boxabhi <abhijeetg40@gmail.com>
2020-12-06 00:11:23 +0530

    url

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import '@/assets/scss/main.scss'
 
-Vue.prototype.$apiUrl = 'https://aasaan-app.com/'
+Vue.prototype.$apiUrl = 'https://aasaan-app.com'
 Vue.prototype.$axios = axios
 
 window.url = 'https://aasaan-app.com/'
